e7daffb8d5fd54df86a2b99bf5e3609babe19e77
Tabish Shaikh <tabish@archive.org>
2021-11-15 01:24:31 +0530

    fix: https://github.com/code-423n4/2021-11-badgerzaps-findings/issues/6

--- contracts/Zap.sol
+++ contracts/Zap.sol
@@ -176,9 +176,6 @@ contract Zap is Initializable, Pausable, AccessControlDefendedBase {
     * @return fee Fee being charged by ibbtc system. Denominated in corresponding sett token
     */
     function calcMintWithRen(uint amount) public view returns(uint poolId, uint idx, uint bBTC, uint fee) {
-        uint _ibbtc;
-        uint _fee;
-
         // poolId=0, idx=0
         (bBTC, fee) = curveLPToIbbtc(0, pools[0].deposit.calc_token_amount([amount,0], true));
     }
@@ -195,9 +192,6 @@ contract Zap is Initializable, Pausable, AccessControlDefendedBase {
     * @return fee Fee being charged by ibbtc system. Denominated in corresponding sett token
     */
     function calcMintWithWbtc(uint amount) public view returns(uint poolId, uint idx, uint bBTC, uint fee) {
-        uint _ibbtc;
-        uint _fee;
-
         // poolId=0
         (bBTC, fee) = curveLPToIbbtc(0, pools[0].deposit.calc_token_amount([0,amount], true));
         idx = 1;
